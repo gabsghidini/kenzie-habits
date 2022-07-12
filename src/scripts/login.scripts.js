2d56d6a2e859b1ed7bd09f4e180a90f17aa801bd
Gustavo Oliveira <gstvo.dev@gmail.com>
2022-07-12 11:29:00 -0300

    Login request and data saved in LocalStorage

---
+++ src/scripts/login.scripts.js
@@ -0,0 +1,22 @@
+import Login from "../controller/login.controller.js";
+
+class entrar {
+  static logIn() {
+      const email = document.getElementById("email").value
+      const password = document.getElementById("password").value
+
+      const data = {
+          "email": email,
+          "password": password
+      }
+      
+      Login.requestLogin(data)
+     
+  }
+}
+const btnLogin = document.querySelector("#btnLogin");
+
+btnLogin.addEventListener("click", function (event) {
+  event.preventDefault();
+  entrar.logIn();
+});
