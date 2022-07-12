1f13dc44cdd6fe34a80e0775892f2c31baebcc93
Sofia Alexandrino <sofiaalage@gmail.com>
2022-07-12 20:39:30 -0300

    Modal Criar Habito

---
+++ src/controller/modal.js
@@ -0,0 +1,40 @@
+import Habit from "../models/habit.models.js";
+import Habits from "./habits.controller.js";
+
+
+document.addEventListener("DOMContentLoaded", addOnClickEvents);
+
+function addOnClickEvents() {
+    let openModalButton = document.querySelector(".open_modal")
+    openModalButton.addEventListener("click", function(){openModal('.modal_habits')})
+
+    let closeModalButton = document.querySelector(".close_card_div")
+    closeModalButton.addEventListener("click", function(){closeModal('.modal_habits')})
+
+    let modalForm = document.querySelector(".modal_form")
+    modalForm.addEventListener("submit", function(e){sendHabits(e)})
+
+}
+
+function closeModal(elementToClose) {
+    document.querySelector(elementToClose).style.display = "none"
+}
+
+function openModal(elementToClose) {
+    document.querySelector(elementToClose).style.display = "flex"
+}
+
+function sendHabits(event){
+    
+    event.preventDefault()
+
+    let titleValue = document.querySelector("#title").value;
+    let descriptionValue = document.querySelector("#description").value;
+    let categoryValue = document.querySelector("#category").value;
+    
+    let habit = new Habit("", titleValue, descriptionValue, categoryValue, "")
+    
+    console.log(Habits.createHabit(habit))
+}
+
+export default {closeModal, openModal, sendHabits}
---
+++ src/models/habit.models.js
@@ -0,0 +1,8 @@
+export default class Habit {
+    constructor (id, tittle, description, category, status) {
+        this.habit_id = id;
+        this.habit_title = tittle;
+        this.habit_description = description;
+        this.habit_category = category;
+    }
+}
