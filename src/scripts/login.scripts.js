f4d08cb182849fa3c04bf9588826f1dd5fe9ec90
Gabs Ghidini <43341520+gabsghidini@users.noreply.github.com>
2022-07-13 09:33:07 -0300

    Feature: Added logout function that clears up the localStorage

--- src/scripts/login.scripts.js
+++ src/scripts/login.scripts.js
@@ -1,22 +1,26 @@
 import Login from "../controller/login.controller.js";
 
 class entrar {
-  static logIn() {
-      const email = document.getElementById("email").value
-      const password = document.getElementById("password").value
+	static logIn() {
+		const email = document.getElementById("email").value;
+		const password = document.getElementById("password").value;
 
-      const data = {
-          "email": email,
-          "password": password
-      }
-      
-      Login.requestLogin(data)
-     
+		const data = {
+			email: email,
+			password: password,
+		};
+
+		Login.requestLogin(data);
+	}
+
+  static logout() {
+    localStorage.clear();
   }
 }
+
 const btnLogin = document.querySelector("#btnLogin");
 
 btnLogin.addEventListener("click", function (event) {
-  event.preventDefault();
-  entrar.logIn();
+	event.preventDefault();
+	entrar.logIn();
 });
