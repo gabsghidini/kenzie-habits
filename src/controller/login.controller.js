4483eb2cd57adb7dfb7d6ed6a3e71517fca948d5
Gabs Ghidini <gabsghidini@gmail.com>
2022-07-14 12:25:51 -0300

    Hotfix: Login fixed

--- src/controller/login.controller.js
+++ src/controller/login.controller.js
@@ -1,28 +1,39 @@
 export default class Login {
-  static base_url = "https://habits-kenzie.herokuapp.com/api/userLogin";
+	static base_url = "https://habits-kenzie.herokuapp.com/api/userLogin";
 
-  static async requestLogin(data) {
-    await fetch(this.base_url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((resp) => {
-        localStorage.setItem(
-          "@kenzie-habits:user",
-          JSON.stringify(resp.response)
-        );
-        localStorage.setItem(
-          "@kenzie-habits:token",
-          JSON.stringify(resp.token)
-        );
-        
-		return resp;
-        
-      })
-      .catch((err) => console.log(err));
-  }
+	static async requestLogin(data) {
+		await fetch(this.base_url, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		})
+			.then((response) => response.json())
+			.then((resp) => {
+
+        console.log(resp);
+        const token = JSON.stringify(resp.token);
+        const response = JSON.stringify(resp.response);
+
+        localStorage.setItem("@kenzie-habits:user", response);
+        localStorage.setItem("@kenzie-habits:token", token);
+
+				return resp;
+			})
+			.catch((err) => window.alert(err));
+	}
 }
+
+/*
+
+{
+    "response": {
+        "usr_name": "Grupo5 Nicoletl",
+        "usr_email": "grupo5Nicole@mail.com",
+        "usr_image": "https://www.google.com/images/Nicole"
+    },
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NTc4MTAwNTcsImV4cCI6MTY1ODQxNDg1Nywic3ViIjoiMzAifQ.YBtPAgdmxI0ecRmAQo1XBD7aeIqABLIQK5ZUM2Swohg"
+}
+
+*/
--- src/scripts/login.scripts.js
+++ src/scripts/login.scripts.js
@@ -1,34 +1,42 @@
 import Login from "../controller/login.controller.js";
 
 class entrar {
-  static logIn() {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+	static logIn() {
+		const email = document.getElementById("email").value;
+		const password = document.getElementById("password").value;
 
-    const data = {
-      email: email,
-      password: password,
-    };
+		const data = {
+			email: email,
+			password: password,
+		};
 
-    Login.requestLogin(data);
-  }
+		Login.requestLogin(data);
+	}
 
-  static logout() {
-    localStorage.clear();
+	static logout() {
+		localStorage.clear();
 
-    window.location.href = "../index.html";
-  }
+		window.location.href = "../index.html";
+	}
 }
 
 const btnLogin = document.querySelector("#btnLogin");
 
 btnLogin.addEventListener("click", function (event) {
-  event.preventDefault();
-  entrar.logIn();
+	event.preventDefault();
+	entrar.logIn();
 
-  if (localStorage.getItem("@kenzie-habits:token" === undefined)) {
-	window.alert("Email ou senha incorreto.");
-} else {
-    window.location.href = "./homepage.html";
-  }
+	const token = localStorage.getItem("@kenzie-habits:token");
+
+	if (token === null) {
+		window.alert("Faça Login para Continuar.");
+	}
+
+	if (token === undefined) {
+		window.alert("Email ou senha incorreto. Tente novamente.");
+	}
+
+	if (token) {
+		window.location.href = "./homepage.html";
+	}
 });
